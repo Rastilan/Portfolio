44fcbe02de6e800a8a4dd20efc714e2c2b6cb361
Rastilan <christoperbhall@gmail.com>
2024-09-14 12:19:54 -0400

    type: update. Added Navbar and set basis for project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+
+  return (
+    <main>
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 right-0 bg-black p-2 z-50">
+        <div className="flex items-start">
+            <h1 className="text-white text-xl font-bold">Chris Hall's Portfolio</h1>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
